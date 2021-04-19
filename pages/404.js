40b3ed633dad49660ab0f6fcbf1de49e679b5794
mike14747 <mike4747@oh.rr.com>
2021-04-19 11:09:38 -0400

    Reformatted the 404 page so it matches the other pages.

--- pages/404.js
+++ pages/404.js
@@ -1,23 +1,18 @@
 const NoMatch = () => {
     return (
-        <article id="no-match">
+        <article>
             <h2 className="error">
                 Error 404!
             </h2>
             <p>An error has occurred.</p>
             <p>The page you are looking for does not exist!</p>
             <style jsx>{`
-                #no-match {
-                    margin: 1.5rem;
-                }
-
                 p {
                     font-size: 120%;
                 }
 
                 .error {
                     color: #dc3545;
-                    margin-bottom: 1rem;
                 }
             `}</style>
         </article>
