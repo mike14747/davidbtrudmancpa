6beac6628439ddd09d9e207a7c447614a7e6e810
mike14747 <mike4747@oh.rr.com>
2021-04-18 21:51:17 -0400

    Added the thumbtack and paypal widgets to the bottom of the homepage main area.

--- pages/index.js
+++ pages/index.js
@@ -2,25 +2,47 @@ import PropTypes from 'prop-types';
 import BlockContent from '@sanity/block-content-to-react';
 import noContainer from '../utils/noContainer';
 
-// import styles from '../styles/home.module.css';
+import styles from '../styles/home.module.css';
 
 const Home = ({ blockContent }) => {
     return (
-        <article>
-            <section>
-                <h2>Overview</h2>
-            </section>
+        <>
+            <article>
+                <section>
+                    <h2>Overview</h2>
+                </section>
 
-            <section className="main-text">
-                {blockContent && blockContent.pageText
-                    ? <BlockContent
-                        blocks={blockContent.pageText}
-                        serializers={noContainer}
-                    />
-                    : <>An error occurred fetching the summary data.</>
-                }
+                <section className="main-text">
+                    {blockContent && blockContent.pageText
+                        ? <BlockContent
+                            blocks={blockContent.pageText}
+                            serializers={noContainer}
+                        />
+                        : <>An error occurred fetching the summary data.</>
+                    }
+                </section>
+            </article>
+
+            <section className={styles.widgets}>
+                <aside className={styles.thumbtack}>
+                    <a className={styles.thumbtackLink} ng-href="https://www.thumbtack.com/-Skokie-IL/service/2576853" target="_blank" href="https://www.thumbtack.com/-Skokie-IL/service/2576853" rel="noreferrer">
+                        <img ng-src="https://static.thumbtackstatic.com/media/pages/profile/standard-widgets/pro-svg/white/2016.svg" src="https://static.thumbtackstatic.com/media/pages/profile/standard-widgets/pro-svg/white/2016.svg" className={styles.thumbtackImage} />
+                    </a>
+                </aside>
+                <aside className={styles.paypal}>
+                    <img src="/images/paypal.png" alt="PayPal" className={styles.paypalLogo} />
+                    <div className={styles.paymentText}>
+                        Make a payment via PayPal:
+                    </div>
+                    <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+                        <input type="hidden" name="cmd" value="_s-xclick" />
+                        <input type="hidden" name="hosted_button_id" value="PUDKSC6R8FCDL" />
+                        <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!" className={styles.buyNow} />
+                        <img alt="Paypal" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1" />
+                    </form>
+                </aside>
             </section>
-        </article>
+        </>
     );
 };
 
