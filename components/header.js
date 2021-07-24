b1005e86716cd783e063e9ad687e0aebe4d4d615
mike14747 <mike4747@oh.rr.com>
2021-07-23 22:51:21 -0400

    Added optional chaining operators to a few places in the header component.

--- components/header.js
+++ components/header.js
@@ -11,19 +11,19 @@ const Header = () => {
         <header className={'container ' + styles.header}>
             <div className={styles.headerLeft}>
                 <h1 className={styles.logoText}>
-                    {headerData && headerData.logoText
+                    {headerData?.logoText
                         ? <>{headerData.logoText}</>
                         : <>David B Trudman CPA</>
                     }
                 </h1>
                 <h6 className={styles.motto}>
-                    {headerData && headerData.motto
+                    {headerData?.motto
                         ? <>{headerData.motto}</>
                         : <>I help you make good choices along your journey</>
                     }
                 </h6>
             </div>
-            {headerData && headerData.imageUrl &&
+            {headerData?.imageUrl &&
                 <div className={styles.headerRight}>
                     <figure aria-label="David B Trudman" className={styles.profileImageFig}>
                         <Image src={headerData.imageUrl + '?w=400&h=400&fit=min'} role="presentation" alt="David B Trudman" layout="fill" />
